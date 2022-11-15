34e0dc61d42c2e8a246461e8e538f0536e630553
Michele Edington <Edington12@yahoo.com>
2022-11-14 20:56:19 -0500

    setup

---
+++ index.js
@@ -0,0 +1,10 @@
+import prompt from "readline-sync";
+import wordBank from "./word-bank.js";
+/*console.log(wordBank[0]);
+let feedback = prompt.question("How's it going? ");
+console.log(feedback);
+if (feedback === "Good") {
+  console.log("I'm glad!");
+} else {
+  console.log("I'm sorry to hear that");
+}*/
